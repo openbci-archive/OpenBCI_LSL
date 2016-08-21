b4063a49b82d49ae0616513b6fe9996082c255c0
gabrielibagon <gabriel.ibagon@gmail.com>
2016-08-21 13:17:20 -0400

    include gitignore, removed junk

--- lsl_openbci.py
+++ lsl_openbci.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+'''
+  lsl_openbci.py
+  ---------------
+
+  LSL is a 
+'''
 
 import sys
 import open_bci_v3 as bci
@@ -21,7 +27,6 @@ class StreamerLSL():
 
       if not GUI:
         self.initialize_board(autodetect=True)
-
       self.init_board_settings()
 
         
@@ -101,104 +106,103 @@ class StreamerLSL():
 
     def begin(self):
 
-        print ("--------------INFO---------------")
-        print ("User serial interface enabled...\n" + \
-            "View command map at http://docs.openbci.com.\n" + \
-            "Type /start to run -- and /stop before issuing new commands afterwards.\n" + \
-            "Type /exit to exit. \n" + \
-            "Board outputs are automatically printed as: \n" +  \
-            "%  <tab>  message\n" + \
-            "$$$ signals end of message")
+      print ("--------------INFO---------------")
+      print ("User serial interface enabled...\n" + \
+        "View command map at http://docs.openbci.com.\n" + \
+        "Type /start to run -- and /stop before issuing new commands afterwards.\n" + \
+        "Type /exit to exit. \n" + \
+        "Board outputs are automatically printed as: \n" +  \
+        "%  <tab>  message\n" + \
+        "$$$ signals end of message")
 
-        print("\n-------------BEGIN---------------")
-        # Init board state
-        # s: stop board streaming; v: soft reset of the 32-bit board (no effect with 8bit board)
-        s = 'sv'
-        # Tell the board to enable or not daisy module
-        if self.board.daisy:
-            s = s + 'C'
+      print("\n-------------BEGIN---------------")
+      # Init board state
+      # s: stop board streaming; v: soft reset of the 32-bit board (no effect with 8bit board)
+      s = 'sv'
+      # Tell the board to enable or not daisy module
+      if self.board.daisy:
+        s = s + 'C'
+      else:
+        s = s + 'c'
+      # d: Channels settings back to default
+      s = s + 'd'
+
+      while(s != "/exit"):
+        # Send char and wait for registers to set
+        if (not s):
+          pass
+        elif("help" in s):
+          print ("View command map at:" + \
+              "http://docs.openbci.com/software/01-OpenBCI_SDK.\n" +\
+              "For user interface: read README or view" + \
+              "https://github.com/OpenBCI/OpenBCI_Python")
+
+        elif self.board.streaming and s != "/stop":
+          print ("Error: the board is currently streaming data, please type '/stop' before issuing new commands.")
         else:
-            s = s + 'c'
-        # d: Channels settings back to default
-        s = s + 'd'
+          # read silently incoming packet if set (used when stream is stopped)
+          flush = False
 
-        while(s != "/exit"):
-            # Send char and wait for registers to set
-            if (not s):
-                pass
-            elif("help" in s):
-                print ("View command map at:" + \
-                    "http://docs.openbci.com/software/01-OpenBCI_SDK.\n" +\
-                    "For user interface: read README or view" + \
-                    "https://github.com/OpenBCI/OpenBCI_Python")
+          if('/' == s[0]):
+            s = s[1:]
+            rec = False  # current command is recognized or fot
 
-            elif self.board.streaming and s != "/stop":
-                print ("Error: the board is currently streaming data, please type '/stop' before issuing new commands.")
+            if("T:" in s):
+              lapse = int(s[string.find(s, "T:")+2:])
+              rec = True
+            elif("t:" in s):
+              lapse = int(s[string.find(s, "t:")+2:])
+              rec = True
             else:
-                # read silently incoming packet if set (used when stream is stopped)
-                flush = False
+              lapse = -1
 
-                if('/' == s[0]):
-                    s = s[1:]
-                    rec = False  # current command is recognized or fot
+            if("start" in s):
+              # start streaming in a separate thread so we could always send commands in here
+              boardThread = threading.Thread(target=self.board.start_streaming,args=(self.send,-1))
+              boardThread.daemon = True # will stop on exit
+              try:
+                boardThread.start()
+                print("Streaming data...")
+              except:
+                raise
+              rec = True
+            elif('test' in s):
+              test = int(s[s.find("test")+4:])
+              self.board.test_signal(test)
+              rec = True
+            elif('stop' in s):
+              self.board.stop()
+              rec = True
+              flush = True
+            if rec == False:
+              print("Command not recognized...")
 
-                    if("T:" in s):
-                        lapse = int(s[string.find(s, "T:")+2:])
-                        rec = True
-                    elif("t:" in s):
-                        lapse = int(s[string.find(s, "t:")+2:])
-                        rec = True
-                    else:
-                        lapse = -1
+            elif s:
+              for c in s:
+                if sys.hexversion > 0x03000000:
+                    self.board.ser.write(bytes(c, 'utf-8'))
+                else:
+                    self.board.ser.write(bytes(c))
+                time.sleep(0.100)
 
-                    if("start" in s):
-                        # start streaming in a separate thread so we could always send commands in here
-                        boardThread = threading.Thread(target=self.board.start_streaming,args=(self.send,-1))
-                        boardThread.daemon = True # will stop on exit
-                        try:
-                            boardThread.start()
-                            print("Streaming data...")
-                        except:
-                                raise
-                        rec = True
-                    elif('test' in s):
-                        test = int(s[s.find("test")+4:])
-                        self.board.test_signal(test)
-                        rec = True
-                    elif('stop' in s):
-                        self.board.stop()
-                        rec = True
-                        flush = True
-                    if rec == False:
-                        print("Command not recognized...")
-
-                elif s:
-                    for c in s:
-                        if sys.hexversion > 0x03000000:
-                            self.board.ser.write(bytes(c, 'utf-8'))
-                        else:
-                            self.board.ser.write(bytes(c))
-                        time.sleep(0.100)
-
+            line = ''
+            time.sleep(0.1) #Wait to see if the board has anything to report
+            while self.board.ser.inWaiting():
+              c = self.board.ser.read().decode('utf-8', errors='replace')
+              line += c
+              time.sleep(0.001)
+              if (c == '\n') and not flush:
                 line = ''
-                time.sleep(0.1) #Wait to see if the board has anything to report
-                while self.board.ser.inWaiting():
-                    c = self.board.ser.read().decode('utf-8', errors='replace')
-                    line += c
-                    time.sleep(0.001)
-                    if (c == '\n') and not flush:
-                        # print('%\t'+line[:-1])
-                        line = ''
 
-                if not flush:
-                    print(line)
+            if not flush:
+              print(line)
 
-            # Take user input
-            #s = input('--> ')
-            if sys.hexversion > 0x03000000:
-                s = input('--> ')
-            else:
-                s = raw_input('--> ')
+          # Take user input
+          #s = input('--> ')
+          if sys.hexversion > 0x03000000:
+            s = input('--> ')
+          else:
+             s = raw_input('--> ')
 
 
 def main(argv):
